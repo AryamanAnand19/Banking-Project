f8e01a36d57b1e1b0d615989a4ba13ff2d292c72
AryamanAnand19 <103181773+AryamanAnand19@users.noreply.github.com>
2024-07-07 20:03:55 +0530

    implement initial file and folder structure

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Serif, Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] , variable: '--font-inter' });
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ['latin'],
+  variable: '--font-ibm-plex-serif',
+  weight: ['400' , '700']
+})
+export const metadata: Metadata = {
+  title: "PaisaX",
+  description: "PaisaX is a modern banking platform for everyone.",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={'${inter.variable} ${ibmPlexSerif.variable}'}>{children}</body>
+    </html>
+  );
+}
